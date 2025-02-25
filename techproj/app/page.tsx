e227b7a648c2e4fb702412d3123992c40c91255c
paulg <pgotthardt@unomaha.edu>
2025-02-25 14:02:20 -0600

    First push

---
+++ techproj/app/page.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Home = () => (
+    <div>
+      <Head>
+        <title>Hello World</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+);
+
+export default Home;
